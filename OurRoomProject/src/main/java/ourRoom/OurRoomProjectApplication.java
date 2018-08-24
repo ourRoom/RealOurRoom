235bfeaf030c4fdc710eac8b50c60abd1ce4c2e0
hanna <hanna@gmail.com>
2018-08-24 17:53:22 +0900

    이제 좀.. 마지막 github..

---
+++ OurRoomProject/src/main/java/ourRoom/OurRoomProjectApplication.java
@@ -0,0 +1,12 @@
+package ourRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurRoomProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurRoomProjectApplication.class, args);
+	}
+}
